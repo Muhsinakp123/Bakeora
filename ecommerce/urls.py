1326d279b220531dd5c6184edb37ecd278116782
Muhsina Musthafa K P <muhsinamusthafakp26@gmail.com>
2026-01-02 14:36:13 +0530

    '02-01-2025'

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('cakes/',views.cakes,name='cakes'),
+    path('puddings/',views.puddings,name='puddings'),
+    path('desserts/',views.desserts,name='desserts'),
+    path('dryfruits/',views.dryfruits,name='dryfruits'),
+    path('sugarfree/',views.sugarfree,name='sugarfree'),
+    path('gift/',views.gift,name='gift'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def cakes(request):
+    return render(request,'cakes.html')
+
+def puddings(request):
+    return render(request,'puddings.html')
+
+def desserts(request):
+    return render(request,'desserts.html')
+
+def dryfruits(request):
+    return render(request,'dryfruits.html')
+
+def sugarfree(request):
+    return render(request,'sugarfree.html')
+
+def gift(request):
+    return render(request,'gift.html')
